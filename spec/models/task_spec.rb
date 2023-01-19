83b1b7f84dc0545829cd0e97f6eb50b4d619b97a
tabithatrubey <tabithatrubey@outlook.com>
2023-01-18 16:07:13 -0800

    finished testing for Task

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should validate task has a name" do
+    task = Task.create
+    expect(task.errors[:name]).to_not be_empty
+  end
+  it "should validate task has a priority" do
+    task = Task.create
+    expect(task.errors[:priority]).to_not be_empty
+  end
 end
