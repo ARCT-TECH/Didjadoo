d0863c1f94d891ac38139c02ab731f29862809be
renitagid <renita.y.ip@gmail.com>
2023-01-19 13:25:29 -0800

    create method for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,17 @@ class TasksController < ApplicationController
         tasks = Task.all 
         render json: tasks
     end
+
+    def create
+        task = Task.create(task_params)
+        if task.valid?
+        render json: task
+        else render json: task.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def task_params
+      params.require(:task).permit(:name, :description, :priority, :progress, :private, :deadline, :user_id)
+    end
 end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -16,3 +16,38 @@ RSpec.describe "Tasks", type: :request do
     end
   end
 end
+
+describe 'POST /create' do
+  it "create a task" do
+    User.create(name: "Renita", email: "hello@world.com", password: "1234hi")
+    user = User.first
+    task_params = {
+
+        name: 'Yoga',
+        priority: 'high',
+        description: 'warrior one',
+        progress: 'not started',
+        deadline: Date.today,
+        private: true,
+        user_id: user.id
+      }
+      
+      post '/tasks', params: {task: task_params}
+      expect(response).to have_http_status(200)
+      task = Task.last
+      expect(task.name).to eq "Yoga"
+    end
+  
+
+  it "doesn't create a task without a name and priority" do
+    User.create(name: "Renita", email: "hello@world.com", password: "1234hi")
+    user = User.first
+      task_params = {
+        user_id: user.id
+      }
+    post '/tasks', params: {task: task_params}
+    expect(response.status).to eq(422)
+    json = JSON.parse(response.body)
+    expect(json['name']).to include "can't be blank"
+  end
+end
