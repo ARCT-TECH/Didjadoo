e75ccabfa26bca43ba7f58409effbd9427be5d58
renitagid <renita.y.ip@gmail.com>
2023-01-25 11:08:59 -0800

    about me bios

--- app/javascript/components/pages/AboutUs.js
+++ app/javascript/components/pages/AboutUs.js
@@ -7,15 +7,17 @@ const AboutUs = () => {
     <div>A team of Didja-dooers, here to help you achieve your goals!</div>
     <div>
       <img src="https://avatars.githubusercontent.com/u/111712184?s=400&u=1d5dedbbaa52cfbd69091cbbb02e84080298ae7e&v=4" />
-      Anthony: (Anthony's Bio)
+      Anthony: I would consider myself a "nerd", when it comes to puzzles. You could say, this trait helps me when it comes to problem solving and trouble shooting. My Teammates found me during our endeavor at LEARN Academy. We Hope you enjoy This App as much as we enjoyed creating it!
     </div>
     <div>
       <img src="https://t4.ftcdn.net/jpg/01/38/35/33/360_F_138353307_iIrtMa8uMNNSxlE4snmCSSJCMkfAfEcD.jpg" />
-      Renita: (Renita's Bio)
+      Renita: Renita is transitioning to web development from a career in behavior sciences, where she mostly worked with children, gathering, analyzing, and reporting data about their progress in ABA therapy. In her free time she loves gardening, crossword puzzles, and participating in speed jigsaw-puzzle competitions, as well as being mom to a 4-year old.
     </div>
     <div>
       <img src="https://pbs.twimg.com/media/CMn4YzSVAAAfhR2.jpg" />
-      Ciani: (Ciani's Bio)
+      Ciani: Hello, I am Ciani Brown, a Junior Full Stack Web Developer. Prior to getting into tech, I was a Personnel Specialist in the United States Navy for 10 years. While on Active Duty, I was stationed and traveled all around the world. I have a Bachelors of Science in Criminal Justice, and a Masters in Business Administration.
+I decided to pivot into tech. With using the skills I acquired in the military, it has made my transition easy.
+I enjoy brainstorming and mapping out new application design and technologies for issues that have once inconvenienced me and others.
     </div>
     <div>
       <img src="https://www.lpzoo.org/wp-content/uploads/2020/07/pre66_Portrait-67.jpg" />
