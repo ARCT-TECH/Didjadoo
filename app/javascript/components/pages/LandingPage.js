a32f6ac1ea9b547b80ea1cc7c91b36c222c8f4ba
tabithatrubey <tabithatrubey@outlook.com>
2023-01-26 10:05:10 -0800

    mock images until other pages for preview are complete

--- app/javascript/components/pages/LandingPage.js
+++ app/javascript/components/pages/LandingPage.js
@@ -1,6 +1,13 @@
 import React from "react";
-import { Button } from "reactstrap";
+import { Button, UncontrolledCarousel } from "reactstrap";
 import YourFriends from "./YourFriends";
+import "./LandingPage.css";
+import didjatest from "../../../assets/images/didjatest.png";
+import kangaroo from "../../../assets/images/kangaroo.png"
+import UncontrolledExample from "./UncontrolledExample";
+
+
+
 
 const LandingPage = ({
   logged_in,
@@ -18,24 +25,38 @@ const LandingPage = ({
   return (
     <>
       {!logged_in && (
-        <>
-          <div>Didjadoo</div>
-          <div className="landing-page-main">
-            Are you meeting your New Year's Resolution goals? Didjadoo helps you
-            track your goals and provides motivation and accountability along
-            the way. Your friends can cheer you on as you complete tasks related
-            to achieving your goals!
-          </div>
+        <>   
+          <div className="video-wrapper">
+           <video playsInline autoPlay muted loop>
+          <source
+           src="https://vod-progressive.akamaized.net/exp=1674757737~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2851%2F11%2F289258217%2F1094756916.mp4~hmac=23abc9c7b32d3fe1dee738ea0e89efab0cbe0a9f3823404688b1107c2cfa3748/vimeo-prod-skyfire-std-us/01/2851/11/289258217/1094756916.mp4?download=1&filename=Pexels+Videos+1409899.mp4"
+           type="video/mp4"
+          />
+        </video>
 
-          <div className="home-buttons">
-            <a href={sign_in_route}>
-              <Button variant="secondary">Sign In </Button>
-            </a>
-            <a href={new_user_route}>
-              <Button variant="secondary">Sign Up </Button>
-            </a>
-          </div>
+      
+             <div className="landing-page-main">
+              <p>Are you meeting your New Year's Resolution goals?</p> 
+              <p>Didjadoo helps you
+                track your goals and provides motivation and accountability along
+                the way. Your friends can cheer you on as you complete tasks related
+                to achieving your goals!</p>
+                <div className="home-buttons">
+                    <a href={new_user_route}>
+                  <Button variant="secondary">Join For Free!</Button>
+                    </a>
+                </div>
+            </div>
+        </div>
+          <img  
+              alt="didjatest"
+              className="didjatest"
+              src={didjatest}
+            />
+            <UncontrolledExample />
+        
         </>
+    
       )}
 
       <div className="home-main">
