75b54c77e304046a9bbd8d6341cd0aad5744899d
Shadow090509 <jamoso090509@gmail.com>
2023-01-26 11:47:44 -0800

    push

--- app/javascript/components/pages/YourFriends.js
+++ app/javascript/components/pages/YourFriends.js
@@ -24,7 +24,7 @@ const YourFriends = ({ users }) => {
             key={index}
           >
             <NavLink to={`/usershow/${users?.id}`}>
-              <img alt="Card" src={users?.profilepic} />
+              <img id="indeximg" alt="Card" src={users?.profilepic} />
             </NavLink>
 
             <CardTitle tag="h5">{users?.name}</CardTitle>
