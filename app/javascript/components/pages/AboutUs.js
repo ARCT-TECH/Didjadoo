861e290b0a2b39b5b3e6afd07056ba4b63e6dfb5
cibrown94 <cianibrown94@aol.com>
2023-01-26 14:39:02 -0800

    changes

--- app/javascript/components/pages/AboutUs.js
+++ app/javascript/components/pages/AboutUs.js
@@ -1,27 +1,55 @@
 import React from "react";
+import "./AboutUs.css"
+import party from "../../../assets/images/party.png"
+
 
 const AboutUs = () => {
   return(
   <>
-    <div>ARCT-TECH</div>
-    <div>A team of Didja-dooers, here to help you achieve your goals!</div>
+    <h1>ARCT-TECH</h1>
+    <h2>A team of Didja-dooers, here to help you achieve your goals!</h2>
+    <div className="wrapper">
+      
+
+   
     <div>
-      <img src="https://avatars.githubusercontent.com/u/111712184?s=400&u=1d5dedbbaa52cfbd69091cbbb02e84080298ae7e&v=4" />
-      Anthony: I would consider myself a "nerd", when it comes to puzzles. You could say, this trait helps me when it comes to problem solving and trouble shooting. My Teammates found me during our endeavor at LEARN Academy. We Hope you enjoy This App as much as we enjoyed creating it!
+    <a href="https://www.linkedin.com/in/anthony-ramirez-b33977240/">
+      <img className="image" src="https://avatars.githubusercontent.com/u/111712184?s=400&u=1d5dedbbaa52cfbd69091cbbb02e84080298ae7e&v=4" /></a>
+      <h3>Anthony</h3>
+       
+       <p className= "bios">I would consider myself a "nerd", when it comes to puzzles. You could say, this trait helps me when it comes to problem solving and trouble shooting. My Teammates found me during our endeavor at LEARN Academy. We Hope you enjoy This App as much as we enjoyed creating it!
+       </p>
+      
+    </div>
+    <div >
+    <a href="https://www.linkedin.com/in/renitagidlund/">
+      <img className="image" src="https://www.renitagidlund.com/-_-/res/2304e9c6-838d-4573-9e32-769ffc5b4e42/images/files/2304e9c6-838d-4573-9e32-769ffc5b4e42/2a09be3e-a7a1-482b-85f4-6acb370ba12f/320-320/7781dff7836659cc312e50b76d999a75dd78d7c4" /></a>
+      <h3>Renita</h3>
+      <p className= "bios">
+       I transitioned to web development from a career in behavior sciences, where I mostly worked with children, gathering, analyzing, and reporting data about their progress in ABA therapy. In my free time she loves gardening, crossword puzzles, and participating in speed jigsaw-puzzle competitions, as well as being mom to a 4-year old!
+       </p> 
     </div>
     <div>
-      <img src="https://t4.ftcdn.net/jpg/01/38/35/33/360_F_138353307_iIrtMa8uMNNSxlE4snmCSSJCMkfAfEcD.jpg" />
-      Renita:  I transitioned to web development from a career in behavior sciences, where I mostly worked with children, gathering, analyzing, and reporting data about their progress in ABA therapy. In my free time she loves gardening, crossword puzzles, and participating in speed jigsaw-puzzle competitions, as well as being mom to a 4-year old! 
-    </div>
-    <div>
-      <img src="https://pbs.twimg.com/media/CMn4YzSVAAAfhR2.jpg" />
-      Ciani: Hello, I am Ciani Brown, a Junior Full Stack Web Developer. Prior to getting into tech, I was a Personnel Specialist in the United States Navy for 10 years. While on Active Duty, I was stationed and traveled all around the world. I have a Bachelors of Science in Criminal Justice, and a Masters in Business Administration.
+    <a href="https://www.linkedin.com/in/ciani-brown/">
+      <img className="image"  src="https://media.licdn.com/dms/image/C4E03AQHnz08jZ57Phg/profile-displayphoto-shrink_400_400/0/1639260274722?e=1680134400&v=beta&t=EN_T_viY-f5xPJjlihXctl5ABcEuOzYWZx6lfIEAHFc" /></a>
+      <h3>Ciani</h3>
+      <p className= "bios">
+       I am a Junior Full Stack Web Developer. Prior to getting into tech, I was a Personnel Specialist in the United States Navy for 10 years. While on Active Duty, I was stationed and traveled all around the world. I have a Bachelors of Science in Criminal Justice, and a Masters in Business Administration.
 I decided to pivot into tech. With using the skills I acquired in the military, it has made my transition easy.
 I enjoy brainstorming and mapping out new application design and technologies for issues that have once inconvenienced me and others.
+
+</p> 
+</div>
+    <div >
+    <a href="https://www.linkedin.com/in/tabitha-trubey/">
+      <img className="image" src="https://media.licdn.com/dms/image/D4D03AQElDd5Hjt4NIw/profile-displayphoto-shrink_400_400/0/1669926382452?e=1680134400&v=beta&t=fTkJv7KuesVoyHsWNG7Ef_KfZq8VaWBmWUq6P9kP6yY"/></a>
+      <h3>Tabi</h3>
+      <p className= "bios">
+       Prior to full stack development, I served in the United States Marine Corps as a Cyber Intelligence Analyst. However, after much soul searching, I made a career jump to Software Development. Software development offers me an opportunity to chase my passions of problem solving, directly impacting mission, and connecting with people to provide them with the exact product they require or want. I am passionate about watching a multiple-page application come to fruition via both front-end and back-end development. Hardened network security is important to me, thus, I take pride in code validations to ensure only certain items and values are permitted. I enjoy the organization and the ability to quickly parse through big data with relational databases, like PostgreSQL. Aside from program functionality, I am enjoy crafting live pages with HTML and CSS!
+       </p> 
+
+       
     </div>
-    <div>
-      <img src="https://www.lpzoo.org/wp-content/uploads/2020/07/pre66_Portrait-67.jpg" />
-      Tabi: Prior to full stack development, I served in the United States Marine Corps as a Cyber Intelligence Analyst. However, after much soul searching, I made a career jump to Software Development. Software development offers me an opportunity to chase my passions of problem solving, directly impacting mission, and connecting with people to provide them with the exact product they require or want. I am passionate about watching a multiple-page application come to fruition via both front-end and back-end development. Hardened network security is important to me, thus, I take pride in code validations to ensure only certain items and values are permitted. I enjoy the organization and the ability to quickly parse through big data with relational databases, like PostgreSQL. Aside from program functionality, I am enjoy crafting live pages with HTML and CSS!
     </div>
   </>
   )
